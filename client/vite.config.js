081b993e5766cbfe4d86a5482eb2ec3204e8967c
Lawrence Quizon <lawrence.quizon@eee.upd.edu.ph>
2026-03-03 14:14:03 +0000

    public vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    allowedHosts: ['lugs-ideapad','verilog.eee.upd.edu.ph'],
+    allowedHosts: ['lugs-ideapad','digital.eee.upd.edu.ph'],
     proxy: {
       '/api': {
         target: 'http://server:3000',
